ebb3339283cca000321e48f9f12eaa97cfad662c
fescobar2018 <38386409+fescobar2018@users.noreply.github.com>
2018-04-14 20:52:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <html>
  <head>
   <title>Prueba de PHP</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="estilo.css" />
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
+  
+  <div class="container">
+ 
+   <?php echo '<p>Hola Mundo</p>'; ?>
   <?php echo '<p>Estes es un laboratorio de Virtualizacion </p>' ?>
+ 
+   
+  </div>
+  
+  
  </body>
 </html>
